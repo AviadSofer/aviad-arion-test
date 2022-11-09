fd68ed01fa793e13e4dc04dd896f507a1e3892ba
guy <guy@arionfund.com>
2022-11-09 13:40:38 +0200

    minor fix

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <h3 >My contact page</h3>
       </header>
       <section className='main-content'>
-        {persons.length > 0 ? <ul className='person-container'> {persons.map(person => <PersonPreview person={person} />)}</ul> : <h1>loading...</h1>}
+        {persons?.length > 0 ? <ul className='person-container'> {persons.map(person => <PersonPreview person={person} />)}</ul> : <h1>loading...</h1>}
       </section>
       <footer className="App-footer"></footer>
     </div >
